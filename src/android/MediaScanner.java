40fbd284d99ed31df229892ccd56f70d0f585c29
Jc Tan <omegachien@gmail.com>
2016-01-15 16:36:55 +0800

    added callback

--- src/android/MediaScanner.java
+++ src/android/MediaScanner.java
@@ -1,46 +1,24 @@
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaInterface;
-import android.util.Log;
-import android.provider.Settings;
-import android.content.Intent;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.util.Log;
-import android.os.Environment;
-import java.lang.InterruptedException;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-public class MediaScanner extends CordovaPlugin implements MediaScannerConnection.MediaScannerConnectionClient {
+public class MediaScanner extends CordovaPlugin implements MediaScannerConnection.MediaScannerConnectionClient{
 
 	private MediaScannerConnection scanner;
 	private String filePath;
+    private CallbackContext cbc;
 
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		if(action.equals("mediaScanner")) {
 			Log.i("Call", "OK, on tente un scan...");
-
+            this.cbc = callbackContext;
 			try {
 				filePath = args.getString(0);		//_Get the path of the media to scan
-
-				MediaScannerConnection.scanFile(this.cordova.getActivity().getApplicationContext(),
-                    new String[] { filePath },
-                    null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                            public void onScanCompleted(String path, Uri uri) {
-                                Log.i("ExternalStorage", "Scanned " + path + ":");
-                                Log.i("ExternalStorage", "-> uri=" + uri);
-                                if(uri == null){
-                                    callbackContext.error("error");
-                                }else{
-                                    callbackContext.success();
-                                }
-                            }
-                });
-
-				// return this.mediaScanner(filePath, callbackContext);
+				return this.mediaScanner(filePath, callbackContext);
 			} catch (JSONException e) {
 				e.printStackTrace();
 		        	callbackContext.error(e.getMessage());
@@ -49,7 +27,6 @@ public class MediaScanner extends CordovaPlugin implements MediaScannerConnectio
 		} else {
 			return false;
 		}
-		return false;
 	}
 
 	private boolean mediaScanner(String mediaPath, CallbackContext callback) {
@@ -67,6 +44,11 @@ public class MediaScanner extends CordovaPlugin implements MediaScannerConnectio
 
     @Override
     public void onScanCompleted(String path, Uri uri) {
+        if(uri != null){
+            cbc.success();
+        }else{
+            cbc.error("error");
+        }
         scanner.disconnect();
     }
 }
